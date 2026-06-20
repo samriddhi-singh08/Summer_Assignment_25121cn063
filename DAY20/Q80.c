511b36e44981d6ececa7b17e180a9237d6a15106
samriddhi-singh08 <25121cn063@glbitm.ac.in>
2026-06-20 14:46:09 +0530

    Add DAY20 programs

---
+++ DAY20/Q80.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[10][10], rows, cols, i, j, sum;
+
+    printf("Enter rows and columns: ");
+    scanf("%d%d", &rows, &cols);
+
+    printf("Enter matrix elements:\n");
+    for(i = 0; i < rows; i++)
+        for(j = 0; j < cols; j++)
+            scanf("%d", &a[i][j]);
+
+    printf("Column-wise sums:\n");
+
+    for(j = 0; j < cols; j++)
+    {
+        sum = 0;
+
+        for(i = 0; i < rows; i++)
+            sum += a[i][j];
+
+        printf("Column %d Sum = %d\n", j + 1, sum);
+    }
+
+    return 0;
+}
