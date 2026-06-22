d24e464924dcfe4fffba0ba10f8dc82d839fa5b6
samriddhi-singh08 <25121cn063@glbitm.ac.in>
2026-06-22 22:38:48 +0530

    Add DAY22 programs

---
+++ DAY22/Q85.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int i, length = 0, flag = 1;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    while(str[length] != '\0' && str[length] != '\n')
+        length++;
+
+    for(i = 0; i < length / 2; i++)
+    {
+        if(str[i] != str[length - i - 1])
+        {
+            flag = 0;
+            break;
+        }
+    }
+
+    if(flag)
+        printf("Palindrome String\n");
+    else
+        printf("Not a Palindrome String\n");
+
+    return 0;
+}
---
+++ DAY22/Q88.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100], result[100];
+    int i, j = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        if(str[i] != ' ')
+        {
+            result[j++] = str[i];
+        }
+    }
+
+    result[j] = '\0';
+
+    printf("String without spaces: %s", result);
+
+    return 0;
+}
